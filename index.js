fc8170d22c47d6edae46f2e9068e829193d333c3
breezy-constructor-6081 <murtaza63@gmail.com>
2019-10-08 07:15:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function sayHiToGrandma(string){
   if(string.toLowerCase()===string){
     return "I can't hear you!";
   }else if(string.toUpperCase(string)===string){
-    return "YES INDEED";
+    return "YES INDEED!";
   }else if('I love you, Grandma.'===string){
     return "I love you, too."
   }
