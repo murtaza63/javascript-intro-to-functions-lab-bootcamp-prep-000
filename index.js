39864573ddb1943d9f7236402f14144ad6540ff4
breezy-constructor-6081 <murtaza63@gmail.com>
2019-10-08 07:03:58 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function shout(string){
+  return string.toUpperCase();
+}
+
+function whisper(string){
+  return  string.toLowerCase();
+}
+
+whisper("HELLO");
+
+function logShout(string){
+  console.log(string)
+}
+logShout('Great');
+
+
