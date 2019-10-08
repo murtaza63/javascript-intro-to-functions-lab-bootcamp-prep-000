ba9119e83ec442bf019334f3027e80da23c5dc7e
breezy-constructor-6081 <murtaza63@gmail.com>
2019-10-08 07:15:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ logWhisper("Hello");
 function sayHiToGrandma(string){
   if(string.toLowerCase()===string){
     return "I can't hear you!";
-  }else if(string.toUpperCase(string)===string){
+  }else if(string.toUpperCase()===string){
     return "YES INDEED!";
   }else if('I love you, Grandma.'===string){
     return "I love you, too."
